295f05d399d61d50cbbbac49675106b688459fda
Krishna Praneet Gudipaty <kgudipaty@umass.edu>
2026-01-27 14:16:34 -0500

    up to date

--- system/plotting.py
+++ system/plotting.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load CSV
-df1 = pd.read_csv("/Users/kgudipaty/Desktop/work/iobt25-demo/system/results/2025-08-20_11:45:18/response_times.csv")
-df2 = pd.read_csv("/Users/kgudipaty/Desktop/work/iobt25-demo/system/results/2025-08-20_11:45:19/response_times.csv")
+df1 = pd.read_csv("/Users/kgudipaty/Desktop/work/iobt25-demo/system/results/exp2-task1/response_times.csv")
+df2 = pd.read_csv("/Users/kgudipaty/Desktop/work/iobt25-demo/system/results/exp2-task2/response_times.csv")
 
 # Convert timestamps to datetime (optional, makes the x-axis readable)
 df1['datetime'] = pd.to_datetime(df1['timestamps'], unit='s')
@@ -16,7 +16,7 @@ plt.plot(df2['datetime'], df2['response_time'], marker='o', linestyle='-', label
 plt.xlabel("Time")
 plt.ylabel("Service Time (s)")
 plt.ylim(0, 2)
-plt.title("Response Time over Time")
+plt.title("Service Time over Time")
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
